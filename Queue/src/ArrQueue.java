4ee9ea8abe3ed72d6791a08e035255ef32943715
niallmc14 <niallmc14@gmail.com>
2019-01-18 11:09:58 +0000

    Added simple comments and runtime exceptions

--- Queue/src/ArrQueue.java
+++ Queue/src/ArrQueue.java
@@ -1,22 +1,36 @@
+/*
+ * Author: Niall Mc Guinness
+ * 
+ * */
+
 
 public class ArrQueue<T> implements Queue<T>{
 	
 	private T[] queue;
 	private int size, front, rear, capacity;
-	private static final int MAX_SIZE = 16;
-
-	public ArrQueue(int capacity) {
-		this.capacity = capacity;
-		
+	
+	//Constructors 
+	
+	public ArrQueue(){
+		capacity = 1000;
 		front = 0;
 		rear = 0;
 		queue = (T[]) new Object[capacity];
 	}
+
+	public ArrQueue(int capacity) {
+		
+		this.capacity = capacity;
+		front = 0;
+		rear = 0;
+		queue = (T[]) new Object[capacity];
+	}
+	
+	//Adds an element to the 'back' of the queue
 	@Override
 	public void enqueue(T data) {
-		// TODO Auto-generated method stub
 		
-		if(size == MAX_SIZE) throw new QueueFullException();
+		if(size == capacity) throw new QueueFullException();
 		
 		queue[rear] = data;
 		rear = (rear + 1) % capacity;
@@ -25,9 +39,12 @@ public class ArrQueue<T> implements Queue<T>{
 		return;
 	}
 
+	//Removes the 'front' element of the queue
 	@Override
 	public T dequeue() {
-		// TODO Auto-generated method stub
+		
+		if(isEmpty()) throw new QueueEmptyException();
+		
 		T data = queue[front];
 		queue[front] = null;
 		front = (front + 1) % capacity;
@@ -35,21 +52,21 @@ public class ArrQueue<T> implements Queue<T>{
 		return data;
 	}
 
+	//Returns the front element of the queue
 	@Override
 	public T front() {
-		// TODO Auto-generated method stub
 		return queue[front];
 	}
 
+	//Returns T/F depending on the statement returned. Checks if Array is empty
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
 		return size == 0;
 	}
-
+	
+	//Returns the size of the Queue Array
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
 		return size;
 	}
 	
@@ -63,17 +80,27 @@ public class ArrQueue<T> implements Queue<T>{
 	}
 	
 	public static void main(String[] args) {
-		ArrQueue<Integer> q = new ArrQueue<Integer>(14);
+		ArrQueue<Integer> q = new ArrQueue<Integer>(20);
 		
 		q.enqueue(10);
-		q.enqueue(10);
-		q.dequeue();
 		System.out.println(q.toString());
 		q.enqueue(20);
+		System.out.println(q.toString());
 		q.enqueue(30);
+		System.out.println(q.toString());
 		q.enqueue(40);
+		System.out.println(q.toString());
 		q.dequeue();
 		System.out.println(q.toString());
+
+		q.dequeue();
+		System.out.println(q.toString());
+		
+		System.out.println("Front element: " + q.front());
+
+		q.dequeue();
+		System.out.println("Front element: " + q.front());
+
 	}
 
 }
