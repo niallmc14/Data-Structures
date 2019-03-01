b7459e75064e36f0cd67ff6ddf008d5fe0e0ad12
niallmc14 <niallmc14@gmail.com>
2019-03-01 22:40:43 +0000

    Delete Vector.java

--- Vectors/src/Vector.java
+++
@@ -1,9 +0,0 @@
-
-public interface Vector<T> {
-    int size();
-    boolean isEmpty();
-    T elemAtRank(int rank);
-    T replaceAtRank(int rank, T element);
-    void insertAtRank(int rank, T element);
-    T removeAtRank(int rank);
-}
